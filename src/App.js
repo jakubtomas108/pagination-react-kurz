27a26d79929aa348ccf1181a9f1b8a00137c1e96
Jakub Tomáš <jakub.tomas@proofreason.com>
2019-11-05 20:53:41 +0100

    Implement simple pagination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const transactionsList = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16
+];
+
+const App = () => {
+    const [transactions] = useState(transactionsList);
+    const [transactionToDisplay, setTransactionsToDisplay] = useState(
+        transactions
+    );
+
+    useEffect(() => {
+        setPagination(0);
+    }, []);
+
+    const setPagination = pageNumber => {
+        setTransactionsToDisplay(
+            transactions.slice(pageNumber * 4, pageNumber * 4 + 4)
+        );
+    };
+
+    return (
+        <div>
+            <p>{transactionToDisplay.map(transaction => `${transaction},`)}</p>
+            <button onClick={() => setPagination(0)}>1</button>
+            <button onClick={() => setPagination(1)}>2</button>
+            <button onClick={() => setPagination(2)}>3</button>
+            <button onClick={() => setPagination(3)}>4</button>
+        </div>
+    );
+};
 
 export default App;
